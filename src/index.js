7a78971fff6c0f673fd5609f9aa2573557108b42
irina-lapkina <infokotostudio@gmail.com>
2024-10-20 21:23:11 +0400

    feat: pass  17 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (bracketsConfig.find((el) => el[0] === char)) {
+      stack.push(char);
+    }
+
+    const bracketsPairArr = bracketsConfig.find((el) => el[1] === char);
+    if (bracketsPairArr) {
+      if (stack[stack.length - 1] !== bracketsPairArr[0]) {
+        return false;
+      }
+      stack.pop(char);
+    }
+  }
+  return stack.length === 0;
+};
